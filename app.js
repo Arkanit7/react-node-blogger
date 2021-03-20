8c7ee55c39ee65599632f2bb4bd435f6568b7c53
Arkanit <61657673+Arkanit7@users.noreply.github.com>
2021-03-20 19:50:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || "5000";
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on http://${HOST}:${PORT}/`);
+  console.log(`Server is listening on ${PORT}`);
 });
 app.get("/*", (req, res) => {
   res.sendFile("./templates/index.html", { root: __dirname });
